cb140c62d927eaa64eb9188eadd212d2c163c1ea
Jefferson <jeffarrubla@gmail.com>
2020-04-07 18:01:28 +0000

    Setting up views and URLs for API

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Moviedata
 
-class MovieSerializer(serializers.Model):
+class MovieSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Moviedata
 		fields = ['id','name','duration','rating']
